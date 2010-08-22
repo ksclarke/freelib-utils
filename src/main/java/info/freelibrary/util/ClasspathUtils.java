e2f5550581619c767a19908e35a4615027cc7fe3
Kevin S. Clarke <ksclarke@gmail.com>
2010-08-22 00:41:02 -0400

    Added javadocs for classpathutils

--- src/main/java/info/freelibrary/util/ClasspathUtils.java
+++ src/main/java/info/freelibrary/util/ClasspathUtils.java
@@ -22,6 +22,12 @@ public class ClasspathUtils {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(ClasspathUtils.class);
 
+	/**
+	 * Returns an String array of all the directory names in the system
+	 * classpath
+	 * 
+	 * @return The names of directories from the system classpath
+	 */
 	public static String[] getDirs() {
 		ArrayList<String> list = new ArrayList<String>();
 
@@ -33,7 +39,7 @@ public class ClasspathUtils {
 						new String[] { file.getAbsolutePath(),
 								file.isDirectory() ? "yes" : "no" });
 			}
-			
+
 			if (file.isDirectory()) {
 				list.add(file.getAbsolutePath());
 			}
@@ -42,6 +48,11 @@ public class ClasspathUtils {
 		return list.toArray(new String[list.size()]);
 	}
 
+	/**
+	 * Returns an array of all the directories in the system classpath
+	 * 
+	 * @return The directories from the system classpath
+	 */
 	public static File[] getDirFiles() {
 		ArrayList<File> list = new ArrayList<File>();
 
@@ -53,7 +64,7 @@ public class ClasspathUtils {
 						new String[] { file.getAbsolutePath(),
 								file.isDirectory() ? "yes" : "no" });
 			}
-			
+
 			if (file.isDirectory()) {
 				list.add(file);
 			}
@@ -62,6 +73,13 @@ public class ClasspathUtils {
 		return list.toArray(new File[list.size()]);
 	}
 
+	/**
+	 * Returns an String array of all the directory names in the system
+	 * classpath that match the supplied <code>FilenameFilter</code>
+	 * 
+	 * @return The names of directories from the system classpath that match the
+	 *         supplied <code>FilenameFilter</code>
+	 */
 	public static String[] getDirs(FilenameFilter aFilter) {
 		ArrayList<String> list = new ArrayList<String>();
 
@@ -77,6 +95,13 @@ public class ClasspathUtils {
 		return list.toArray(new String[list.size()]);
 	}
 
+	/**
+	 * Returns an array of all the directories in the system classpath that
+	 * match the supplied <code>FilenameFilter</code>
+	 * 
+	 * @return The directories from the system classpath that match the supplied
+	 *         <code>FilenameFilter</code>
+	 */
 	public static File[] getDirFiles(FilenameFilter aFilter) {
 		ArrayList<File> list = new ArrayList<File>();
 
@@ -102,6 +127,12 @@ public class ClasspathUtils {
 		return list.toArray(new File[list.size()]);
 	}
 
+	/**
+	 * Returns an String array of all the names of the jars in the system
+	 * classpath
+	 * 
+	 * @return The names of jars from the system classpath
+	 */
 	public static String[] getJars() {
 		ArrayList<String> list = new ArrayList<String>();
 		FileExtFileFilter filter = new FileExtFileFilter("jar");
@@ -117,6 +148,13 @@ public class ClasspathUtils {
 		return list.toArray(new String[list.size()]);
 	}
 
+	/**
+	 * Returns an String array of all the names of the jars in the system
+	 * classpath that match the supplied <code>FilenameFilter</code>
+	 * 
+	 * @return The names of jars from the system classpath that match the
+	 *         supplied <code>FilenameFilter</code>
+	 */
 	public static String[] getJars(FilenameFilter aFilter) {
 		ArrayList<String> list = new ArrayList<String>();
 		FileExtFileFilter filter = new FileExtFileFilter("jar");
@@ -134,6 +172,11 @@ public class ClasspathUtils {
 		return list.toArray(new String[list.size()]);
 	}
 
+	/**
+	 * Returns an array of all the jar files in the system classpath
+	 * 
+	 * @return The jar files from the system classpath
+	 */
 	public static JarFile[] getJarFiles() throws IOException {
 		ArrayList<JarFile> list = new ArrayList<JarFile>();
 		FileExtFileFilter filter = new FileExtFileFilter("jar");
@@ -149,6 +192,13 @@ public class ClasspathUtils {
 		return list.toArray(new JarFile[list.size()]);
 	}
 
+	/**
+	 * Returns an array of all the jar files in the system classpath that match
+	 * the supplied <code>FilenameFilter</code>
+	 * 
+	 * @return The jar files from the system classpath that match the supplied
+	 *         <code>FilenameFilter</code>
+	 */
 	public static JarFile[] getJarFiles(FilenameFilter aFilter)
 			throws IOException {
 		ArrayList<JarFile> list = new ArrayList<JarFile>();
@@ -167,6 +217,14 @@ public class ClasspathUtils {
 		return list.toArray(new JarFile[list.size()]);
 	}
 
+	/**
+	 * Finds the first instance of the supplied file name in the classpath (in
+	 * either a directory or jar file) and returns its <code>InputStream</code>
+	 * 
+	 * @param aFilename The name of the file we want to read
+	 * @return The <code>InputStream</code> of the file we want to read
+	 * @throws IOException If the <code>InputStream</code> can't be read
+	 */
 	public static InputStream findFirst(String aFilename) throws IOException {
 		FileExtFileFilter filter = new FileExtFileFilter("jar");
 
