f645c2e3fb96838e9101f9ca6f907c22d6952460
Kevin S. Clarke <ksclarke@gmail.com>
2010-10-13 16:54:46 -0400

    added a simple i18n abstraction class

---
+++ src/main/java/info/freelibrary/util/I18nObject.java
@@ -0,0 +1,54 @@
+package info.freelibrary.util;
+
+import info.freelibrary.util.XMLBundleControl;
+import info.freelibrary.util.XMLResourceBundle;
+
+import java.io.File;
+import java.util.ResourceBundle;
+
+public abstract class I18nObject {
+
+	private static final XMLResourceBundle BUNDLE = (XMLResourceBundle) ResourceBundle
+			.getBundle("Messages", new XMLBundleControl());
+
+	protected String getI18n(String aMessage) {
+		return BUNDLE.get(aMessage);
+	}
+
+	protected String getI18n(String aMessage, String aDetail) {
+		return BUNDLE.get(aMessage, aDetail);
+	}
+
+	protected String getI18n(String aMessage, String[] aDetailsArray) {
+		return BUNDLE.get(aMessage, aDetailsArray);
+	}
+
+	protected String getI18n(String aMessage, File aFile) {
+		return BUNDLE.get(aMessage, aFile.getAbsolutePath());
+	}
+
+	protected String getI18n(String aMessage, File[] aFileArray) {
+		String[] fileNames = new String[aFileArray.length];
+
+		for (int index = 0; index < fileNames.length; index++) {
+			fileNames[index] = aFileArray[index].getAbsolutePath();
+		}
+
+		return BUNDLE.get(aMessage, fileNames);
+	}
+
+	protected String getI18n(String aMessage, Object[] aObjArray) {
+		String[] strings = new String[aObjArray.length];
+
+		for (int index = 0; index < aObjArray.length; index++) {
+			if (aObjArray[index] instanceof File) {
+				strings[index] = ((File) aObjArray[index]).getAbsolutePath();
+			}
+			else {
+				strings[index] = aObjArray[index].toString();
+			}
+		}
+
+		return BUNDLE.get(aMessage, strings);
+	}
+}
