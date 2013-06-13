755ed0483a3634071c8bd4e67e77390d90f37422
Kevin S. Clarke <ksclarke@gmail.com>
2013-06-13 01:16:21 -0400

    fixed file permissions so files are 644

--- src/main/java/info/freelibrary/util/FileUtils.java
+++ src/main/java/info/freelibrary/util/FileUtils.java
@@ -117,8 +117,7 @@ public class FileUtils implements FileUtilConstants {
 	 * (and all its subdirectories). Includes absolute path, last modified time,
 	 * read/write permissions, etc.
 	 * 
-	 * @param aFilePath The file's or directory's structure to be returned as
-	 *        XML
+	 * @param aFilePath The file or directory to be returned as XML
 	 * @param aPattern A regular expression that file names must match
 	 * @return A string of XML describing the supplied file system path's
 	 *         structure
@@ -132,38 +131,106 @@ public class FileUtils implements FileUtilConstants {
 		return toXML(aFilePath, aPattern, false);
 	}
 
+	/**
+	 * Creates a string of XML that describes the supplied file or directory
+	 * (and, optionally, all its subdirectories). Includes absolute path, last
+	 * modified time, read/write permissions, etc.
+	 * 
+	 * @param aFilePath The file or directory to be returned as XML
+	 * @param aDeepConversion Whether the subdirectories are included
+	 * @return A string of XML describing the supplied file system path's
+	 *         structure
+	 * @throws FileNotFoundException If the supplied file or directory can not
+	 *         be found
+	 * @throws ParserConfigurationException If the default XML parser for the
+	 *         JRE isn't configured correctly
+	 */
 	public static String toXML(String aFilePath, boolean aDeepConversion)
 			throws FileNotFoundException, ParserConfigurationException {
 		return toXML(aFilePath, ".*", aDeepConversion);
 	}
 
+	/**
+	 * Creates a string of XML that describes the supplied file or directory
+	 * (and, optionally, all its subdirectories). Includes absolute path, last
+	 * modified time, read/write permissions, etc.
+	 * 
+	 * @param aFilePath The file or directory to be returned as XML
+	 * @param aPattern A regular expression pattern to evaluate file matches
+	 *        against
+	 * @param aDeepConversion Whether the subdirectories are included
+	 * @return A string of XML describing the supplied file system path's
+	 *         structure
+	 * @throws FileNotFoundException If the supplied file or directory can not
+	 *         be found
+	 * @throws ParserConfigurationException If the default XML parser for the
+	 *         JRE isn't configured correctly
+	 */
 	public static String toXML(String aFilePath, String aPattern,
 			boolean aDeepConversion) throws FileNotFoundException,
 			ParserConfigurationException {
 		return DOMUtils.toXML(toElement(aFilePath, aPattern, aDeepConversion));
 	}
 
+	/**
+	 * Returns a Map representation of the supplied directory's structure. The
+	 * map contains the file name as the key and its path as the value. If a
+	 * file with a name occurs more than once, multiple path values are returned
+	 * for that file name key. The map that is returned is unmodifiable.
+	 * 
+	 * @param aFilePath The directory of which you'd like a file listing
+	 * @return An unmodifiable map representing the files in the file structure
+	 * @throws FileNotFoundException If the directory for the supplied file path
+	 *         does not exist
+	 */
 	public static Map<String, List<String>> toHashMap(String aFilePath)
 			throws FileNotFoundException {
 		return toHashMap(aFilePath, null, null);
 	}
 
-	public static Map<String, List<String>> toHashMap(String aFilePath, String aPattern)
-			throws FileNotFoundException {
+	/**
+	 * Returns a Map representation of the supplied directory's structure. The
+	 * map contains the file name as the key and its path as the value. If a
+	 * file with a name occurs more than once, multiple path values are returned
+	 * for that file name key. The map that is returned is unmodifiable.
+	 * 
+	 * @param aFilePath The directory of which you'd like a file listing
+	 * @param aPattern A regular expression pattern which the files must match
+	 *        to be returned
+	 * @return An unmodifiable map representing the files in the file structure
+	 * @throws FileNotFoundException If the directory for the supplied file path
+	 *         does not exist
+	 */
+	public static Map<String, List<String>> toHashMap(String aFilePath,
+			String aPattern) throws FileNotFoundException {
 		return toHashMap(aFilePath, aPattern, null);
 	}
 
+	/**
+	 * Returns a Map representation of the supplied directory's structure. The
+	 * map contains the file name as the key and its path as the value. If a
+	 * file with a name occurs more than once, multiple path values are returned
+	 * for that file name key. The map that is returned is unmodifiable.
+	 * 
+	 * @param aFilePath The directory of which you'd like a file listing
+	 * @param aPattern A regular expression pattern which the files must match
+	 *        to be returned
+	 * @param aIgnoreList A list of directories into which we shouldn't descend
+	 * @return An unmodifiable map representing the files in the file structure
+	 * @throws FileNotFoundException If the directory for the supplied file path
+	 *         does not exist
+	 */
 	public static Map<String, List<String>> toHashMap(String aFilePath,
 			String aPattern, String[] aIgnoreList) throws FileNotFoundException {
 		String filePattern = aPattern != null ? aPattern : ".*";
 		RegexFileFilter filter = new RegexFileFilter(filePattern);
 		Map<String, List<String>> fileMap = new HashMap();
 		File source = new File(aFilePath);
-		
+
 		for (File file : listFiles(source, filter, true, aIgnoreList)) {
 			String fileName = file.getName();
 			String filePath = file.getAbsolutePath();
-			
+
 			if (fileMap.containsKey(fileName)) {
 				List<String> paths = fileMap.get(fileName);
 
@@ -184,11 +251,43 @@ public class FileUtils implements FileUtilConstants {
 		return Collections.unmodifiableMap(fileMap);
 	}
 
+	/**
+	 * Returns an XML Element representing the file structure found at the
+	 * supplied file system path. Files included in the representation will
+	 * match the supplied regular expression pattern. This method doesn't
+	 * descend through the directory structure.
+	 * 
+	 * @param aFilePath The directory from which the structural representation
+	 *        should be built
+	 * @param aPattern A regular expression pattern which files included in the
+	 *        Element should match
+	 * @return An XML Element representation of the directory structure
+	 * @throws FileNotFoundException If the supplied directory isn't found
+	 * @throws ParserConfigurationException If the default XML parser for the
+	 *         JRE isn't configured correctly
+	 */
 	public static Element toElement(String aFilePath, String aPattern)
 			throws FileNotFoundException, ParserConfigurationException {
 		return toElement(aFilePath, aPattern, false);
 	}
 
+	/**
+	 * Returns an XML Element representing the file structure found at the
+	 * supplied file system path. Files included in the representation will
+	 * match the supplied regular expression pattern. This method doesn't
+	 * descend through the directory structure.
+	 * 
+	 * @param aFilePath The directory from which the structural representation
+	 *        should be built
+	 * @param aPattern A regular expression pattern which files included in the
+	 *        Element should match
+	 * @param aDeepConversion Whether the conversion should descend through
+	 *        subdirectories
+	 * @return An XML Element representation of the directory structure
+	 * @throws FileNotFoundException If the supplied directory isn't found
+	 * @throws ParserConfigurationException If the default XML parser for the
+	 *         JRE isn't configured correctly
+	 */
 	public static Element toElement(String aFilePath, String aPattern,
 			boolean aDeepConversion) throws FileNotFoundException,
 			ParserConfigurationException {
@@ -202,11 +301,43 @@ public class FileUtils implements FileUtilConstants {
 		throw new FileNotFoundException(aFilePath);
 	}
 
+	/**
+	 * Returns an XML Document representing the file structure found at the
+	 * supplied file system path. Files included in the representation will
+	 * match the supplied regular expression pattern. This method doesn't
+	 * descend through the directory structure.
+	 * 
+	 * @param aFilePath The directory from which the structural representation
+	 *        should be built
+	 * @param aPattern A regular expression pattern which files included in the
+	 *        Element should match
+	 * @return An XML Document representation of the directory structure
+	 * @throws FileNotFoundException If the supplied directory isn't found
+	 * @throws ParserConfigurationException If the default XML parser for the
+	 *         JRE isn't configured correctly
+	 */
 	public static Document toDocument(String aFilePath, String aPattern)
 			throws FileNotFoundException, ParserConfigurationException {
 		return toDocument(aFilePath, aPattern, false);
 	}
 
+	/**
+	 * Returns an XML Document representing the file structure found at the
+	 * supplied file system path. Files included in the representation will
+	 * match the supplied regular expression pattern. This method doesn't
+	 * descend through the directory structure.
+	 * 
+	 * @param aFilePath The directory from which the structural representation
+	 *        should be built
+	 * @param aPattern A regular expression pattern which files included in the
+	 *        Document should match
+	 * @param aDeepConversion Whether the conversion should descend through
+	 *        subdirectories
+	 * @return An XML Document representation of the directory structure
+	 * @throws FileNotFoundException If the supplied directory isn't found
+	 * @throws ParserConfigurationException If the default XML parser for the
+	 *         JRE isn't configured correctly
+	 */
 	public static Document toDocument(String aFilePath, String aPattern,
 			boolean aDeepConversion) throws FileNotFoundException,
 			ParserConfigurationException {
@@ -214,6 +345,14 @@ public class FileUtils implements FileUtilConstants {
 		return element.getOwnerDocument();
 	}
 
+	/**
+	 * Returns a Java <code>File</code> for the supplied file-based URL.
+	 * 
+	 * @param aURL A URL that has a file protocol
+	 * @return A Java <code>File</code> for the supplied URL
+	 * @throws MalformedURLException If the supplied URL doesn't have a file
+	 *         protocol
+	 */
 	public static File toFile(URL aURL) throws MalformedURLException {
 		if (aURL.getProtocol().equals("file")) {
 			return new File(aURL.toString().replace("file:", ""));
@@ -222,16 +361,47 @@ public class FileUtils implements FileUtilConstants {
 		throw new MalformedURLException("Not a file URL");
 	}
 
+	/**
+	 * An array of all the files in the supplied directory that match the
+	 * supplied <code>FilenameFilter</code>.
+	 * 
+	 * @param aDir A directory from which a file listing should be returned
+	 * @param aFilter A file name filter which returned files should match
+	 * @return An array of matching files
+	 * @throws FileNotFoundException If the supplied directory doesn't exist
+	 */
 	public static File[] listFiles(File aDir, FilenameFilter aFilter)
 			throws FileNotFoundException {
 		return listFiles(aDir, aFilter, false, null);
 	}
 
+	/**
+	 * An array of all the files in the supplied directory that match the
+	 * supplied <code>FilenameFilter</code>.
+	 * 
+	 * @param aDir A directory from which a file listing should be returned
+	 * @param aFilter A file name filter which returned files should match
+	 * @param aDeepListing Whether we should descend through subdirectories
+	 * @return An array of matching files
+	 * @throws FileNotFoundException If the supplied directory doesn't exist
+	 */
 	public static File[] listFiles(File aDir, FilenameFilter aFilter,
 			boolean aDeepListing) throws FileNotFoundException {
 		return listFiles(aDir, aFilter, aDeepListing, null);
 	}
 
+	/**
+	 * An array of all the files in the supplied directory that match the
+	 * supplied <code>FilenameFilter</code>. Directories that match the supplied
+	 * ignore list will not be included in the result.
+	 * 
+	 * @param aDir A directory from which a file listing should be returned
+	 * @param aFilter A file name filter which returned files should match
+	 * @param aDeepListing Whether we should descend through subdirectories
+	 * @param aIgnoreList Directory names that should be ignored in the list.
+	 * @return An array of matching files
+	 * @throws FileNotFoundException If the supplied directory doesn't exist
+	 */
 	public static File[] listFiles(File aDir, FilenameFilter aFilter,
 			boolean aDeepListing, String[] aIgnoreList)
 			throws FileNotFoundException {
@@ -287,18 +457,30 @@ public class FileUtils implements FileUtilConstants {
 		}
 	}
 
+	/**
+	 * Return a file name without the dot extension.
+	 * 
+	 * @param aFile The file name
+	 * @return The file name without the extension
+	 */
 	public static String stripExt(File aFile) {
 		return stripExt(aFile.getName());
 	}
 
-	public static String stripExt(String aFilename) {
-		int index = aFilename.lastIndexOf('.');
+	/**
+	 * Return a file name without the dot extension.
+	 * 
+	 * @param aFileName The file name from which we want to strip the extension
+	 * @return The file name without the extension
+	 */
+	public static String stripExt(String aFileName) {
+		int index = aFileName.lastIndexOf('.');
 
 		if (index != -1) {
-			return aFilename.substring(0, index);
+			return aFileName.substring(0, index);
 		}
 
-		return aFilename;
+		return aFileName;
 	}
 
 	/**
@@ -350,6 +532,16 @@ public class FileUtils implements FileUtilConstants {
 		return aDir.delete();
 	}
 
+	/**
+	 * Copies a file or directory from one place to another. This copies a file
+	 * to a file or a directory to a directory. It does not copy a file to a
+	 * directory.
+	 * 
+	 * @param aFromFile A file or directory source
+	 * @param aToFile A file or directory destination
+	 * @throws IOException If there is an exception copying the files or
+	 *         directories
+	 */
 	public static void copy(File aFromFile, File aToFile) throws IOException {
 		if ((aFromFile.isDirectory() && aToFile.isFile())
 				|| (aFromFile.isFile() && aToFile.isDirectory())) {
@@ -372,10 +564,25 @@ public class FileUtils implements FileUtilConstants {
 		}
 	}
 
+	/**
+	 * Returns a human readable size from a large number of bytes.
+	 * 
+	 * @param aByteCount A large number of bytes
+	 * @return A human readable size
+	 */
 	public static String sizeFromBytes(long aByteCount) {
 		return sizeFromBytes(aByteCount, false);
 	}
 
+	/**
+	 * Returns a human readable size from a large number of bytes. You can
+	 * specify that the human readable size use an abbreviated label (e.g., GB
+	 * or MB).
+	 * 
+	 * @param aByteCount A large number of bytes
+	 * @param aAbbreviatedLabel Whether the label should be abbreviated
+	 * @return A human readable size
+	 */
 	public static String sizeFromBytes(long aByteCount,
 			boolean aAbbreviatedLabel) {
 		long count;
@@ -393,6 +600,16 @@ public class FileUtils implements FileUtilConstants {
 		return count + (aAbbreviatedLabel ? " B" : " bytes");
 	}
 
+	/**
+	 * Get a hash for a supplied file.
+	 * 
+	 * @param aFile A file to get a hash for
+	 * @param aAlgorithm A hash algorithm supported by <code>MessageDigest</code>
+	 * @return A hash string
+	 * @throws NoSuchAlgorithmException If the supplied algorithm isn't supported
+	 * @throws FileNotFoundException If the supplied file doesn't exist
+	 * @throws IOException If there is trouble reading the file
+	 */
 	public static String hash(File aFile, String aAlgorithm)
 			throws NoSuchAlgorithmException, FileNotFoundException, IOException {
 		MessageDigest md = MessageDigest.getInstance(aAlgorithm);
@@ -410,6 +627,13 @@ public class FileUtils implements FileUtilConstants {
 		return formatter.toString();
 	}
 
+	/**
+	 * Gets the MIME type of the supplied file URL.  It gets MIME type from <code>URLConnection</code>'s file name map.
+	 * 
+	 * @param aFileUrl A file-based URL
+	 * @return
+	 * @throws IOException
+	 */
 	public static String getMimeType(String aFileUrl) throws IOException {
 		return URLConnection.getFileNameMap().getContentTypeFor(aFileUrl);
 	}
