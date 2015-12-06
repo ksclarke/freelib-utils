8435a80ac4e55fbc3f8f2ac6842d7352c749f0a3
Kevin S. Clarke <ksclarke@gmail.com>
2015-12-06 10:49:23 -0500

    Add option to process more than one file/dir with FilePermissionsMojo

--- src/main/java/info/freelibrary/maven/FilePermissionsMojo.java
+++ src/main/java/info/freelibrary/maven/FilePermissionsMojo.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermission;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -13,15 +15,11 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import info.freelibrary.util.FileUtils;
 
 @Mojo(name = "set-file-perms")
 public class FilePermissionsMojo extends AbstractMojo {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(FilePermissionsMojo.class);
 
     /**
      * The Maven project directory.
@@ -36,17 +34,31 @@ public class FilePermissionsMojo extends AbstractMojo {
     private int myPerms;
 
     /**
-     * The file or directory to set permissions.
+     * The file or directory on which to set permissions.
      */
     @Parameter(alias = "file")
     private File myFile;
+
+    /**
+     * A list of files or directories on which to set permissions.
+     */
+    @Parameter(alias = "files")
+    private List<String> myFiles;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         final Set<PosixFilePermission> perms = FileUtils.convertToPermissionsSet(myPerms);
 
         try {
-            Files.setPosixFilePermissions(myFile.toPath(), perms);
+            if (myFile != null) {
+                Files.setPosixFilePermissions(myFile.toPath(), perms);
+            } else if (myFiles != null) {
+                final Iterator<String> iterator = myFiles.iterator();
+
+                while (iterator.hasNext()) {
+                    Files.setPosixFilePermissions(new File(iterator.next()).toPath(), perms);
+                }
+            }
         } catch (final IOException details) {
             throw new MojoExecutionException(details.getMessage(), details);
         }
