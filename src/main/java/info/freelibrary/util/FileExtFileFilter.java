1b2e55b25dd3fbd1816b8e82c222d2d1777f8fe7
Kevin S. Clarke <ksclarke@ksclarke.io>
2016-02-25 22:58:29 -0500

    Fix file extension parsing bug

--- src/main/java/info/freelibrary/util/FileExtFileFilter.java
+++ src/main/java/info/freelibrary/util/FileExtFileFilter.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 /**
  * A file name filter that checks the file extension to determine whether the filter matches the file name or not. It
  * can have a single or multiple file extensions in the form: 'gif' or 'xml'.
- * 
+ *
  * @author <a href="mailto:ksclarke@gmail.com">Kevin S. Clarke</a>
  */
 public class FileExtFileFilter implements FilenameFilter {
@@ -22,32 +22,32 @@ public class FileExtFileFilter implements FilenameFilter {
     /**
      * Constructor for a <code>FilenameFilter</code> that checks a single file extension against supplied file names,
      * looking for matches.
-     * 
+     *
      * @param aFileExt A file extensions (minus the '.') against which we want to compare
      */
     public FileExtFileFilter(final String aFileExt) {
         myExtensions = new String[] {
-            "." + aFileExt
+            normalizeExt(aFileExt)
         };
     }
 
     /**
      * Constructor for a <code>FilenameFilter</code> that checks an array of file extensions against supplied file
      * names, looking for matches.
-     * 
+     *
      * @param aFileExtList A list of file extensions (minus the '.') against which we want to compare
      */
     public FileExtFileFilter(final String... aFileExtList) {
         myExtensions = new String[aFileExtList.length];
 
         for (int index = 0; index < aFileExtList.length; index++) {
-            myExtensions[index] = "." + aFileExtList[index];
+            myExtensions[index] = normalizeExt(aFileExtList[index]);
         }
     }
 
     /**
      * Returns a string representation of the filter which includes the file name extensions being used as filters.
-     * 
+     *
      * @return A string representation of the filter which includes the file name extensions being used as filters.
      */
     @Override
@@ -57,7 +57,7 @@ public class FileExtFileFilter implements FilenameFilter {
 
     /**
      * Returns true if the supplied file name and parent directory are a match for this <code>FilenameFilter</code>.
-     * 
+     *
      * @param aDir A parent directory for the supplied file name
      * @param aFileName The file name we want to check against our filter
      * @return True if the filter matches the supplied parent and file name; else, false
@@ -75,17 +75,24 @@ public class FileExtFileFilter implements FilenameFilter {
 
     /**
      * Returns whether or not the supplied file extension is one that this filter matches.
-     * 
+     *
      * @param aFileExt A file extension like: jpg, gif, jp2, txt, xml, etc.
      * @return True if this filter matches files with the supplied extension
      */
     public boolean filters(final String aFileExt) {
+        final String normalizedExt = normalizeExt(aFileExt);
+
         for (final String extension : myExtensions) {
-            if (extension.equals("." + aFileExt)) {
+            if (extension.equals(normalizedExt)) {
                 return true;
             }
         }
 
         return false;
     }
+
+    private String normalizeExt(final String aFileExt) {
+        return "." + (aFileExt.startsWith(".") ? aFileExt.substring(1) : aFileExt);
+    }
+
 }
---
+++ src/test/java/info/freelibrary/util/FileExtFileFilterTest.java
@@ -0,0 +1,48 @@
+package info.freelibrary.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+
+public class FileExtFileFilterTest {
+
+    @Test
+    public final void testFileExtFileFilterString() {
+        assertEquals(new FileExtFileFilter(".jpg").toString(), "[.jpg]");
+        assertEquals(new FileExtFileFilter("jpg").toString(), "[.jpg]");
+    }
+
+    @Test
+    public final void testFileExtFileFilterStringArray() {
+        assertEquals(new FileExtFileFilter(".jpg", "tif").toString(), "[.jpg, .tif]");
+        assertEquals(new FileExtFileFilter("jpg", ".tif").toString(), "[.jpg, .tif]");
+    }
+
+    @Test
+    public final void testToString() {
+        assertEquals(new FileExtFileFilter("jpg", "tif").toString(), "[.jpg, .tif]");
+    }
+
+    @Test
+    public final void testAccept() {
+        final String[] fileNames = new File("src/test/resources").list(new FileExtFileFilter("txt"));
+
+        assertEquals(1, fileNames.length);
+        assertEquals("80_char_test_1.txt", fileNames[0]);
+    }
+
+    @Test
+    public final void testFilters() {
+        assertTrue(new FileExtFileFilter(".jpg", "tif").filters("tif"));
+        assertTrue(new FileExtFileFilter("jpg", ".tif").filters("tif"));
+        assertTrue(new FileExtFileFilter(".jpg", "tif").filters(".tif"));
+        assertTrue(new FileExtFileFilter("jpg", ".tif").filters(".tif"));
+        assertTrue(new FileExtFileFilter("tif").filters(".tif"));
+        assertTrue(new FileExtFileFilter(".tif").filters(".tif"));
+    }
+
+}
