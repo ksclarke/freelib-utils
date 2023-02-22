f33878b873eb4621704b4a933f66664a762a7c95
Kevin S. Clarke <ksclarke@ksclarke.io>
2023-02-21 22:22:17 -0500

    Update parent project; add NumberUtils

--- src/main/java/info/freelibrary/util/warnings/Checkstyle.java
+++ src/main/java/info/freelibrary/util/warnings/Checkstyle.java
@@ -7,9 +7,24 @@ package info.freelibrary.util.warnings;
 public final class Checkstyle {
 
     /**
+     * Cf. https://checkstyle.sourceforge.io/config_imports.html#AvoidStarImport
+     */
+    public static final String AVOID_STAR_IMPORT = "checkstyle:AvoidStarImport";
+
+    /**
      * Cf. https://checkstyle.sourceforge.io/config_coding.html#MultipleStringLiterals
      */
-    public static final String MULTIPLE_STRING_LITERALS = "MultipleStringLiterals";
+    public static final String MULTIPLE_STRING_LITERALS = "checkstyle:MultipleStringLiterals";
+
+    /**
+     * Cf. https://checkstyle.sourceforge.io/config_metrics.html#BooleanExpressionComplexity
+     */
+    public static final String BOOLEAN_EXPR_COMPLEXITY = "checkstyle:BooleanExpressionComplexity";
+
+    /**
+     * Cf. https://checkstyle.sourceforge.io/config_naming.html#MemberName
+     */
+    public static final String MEMBER_NAME = "checkstyle:MemberName";
 
     /**
      * Creates a new Checkstyle warnings class.
