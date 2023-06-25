d6f6be1cc01a3d494456652cef85946fc67866c4
Kevin S. Clarke <ksclarke@ksclarke.io>
2023-06-25 17:09:27 -0400

    Add Labeled interface (#66)

---
+++ src/main/java/info/freelibrary/util/Labeled.java
@@ -0,0 +1,15 @@
+
+package info.freelibrary.util;
+
+/**
+ * An interface for enumerations that standardizes accessing the enum's label.
+ */
+public interface Labeled {
+
+    /**
+     * Gets the enumeration's label.
+     *
+     * @return A label
+     */
+    String label();
+}
