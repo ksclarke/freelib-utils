63f45cbe48ba336b7a6d8e097b0fdcc7e3d8335c
Kevin S. Clarke <ksclarke@ksclarke.io>
2025-01-21 21:47:08 -0500

    Update warnings

--- src/main/java/info/freelibrary/util/warnings/Checkstyle.java
+++ src/main/java/info/freelibrary/util/warnings/Checkstyle.java
@@ -6,25 +6,20 @@ package info.freelibrary.util.warnings;
  */
 public final class Checkstyle {
 
-    /**
-     * Cf. https://checkstyle.sourceforge.io/config_imports.html#AvoidStarImport
-     */
+    /** Cf. https://checkstyle.sourceforge.io/config_imports.html#AvoidStarImport */
     public static final String AVOID_STAR_IMPORT = "checkstyle:AvoidStarImport";
 
-    /**
-     * Cf. https://checkstyle.sourceforge.io/config_coding.html#MultipleStringLiterals
-     */
-    public static final String MULTIPLE_STRING_LITERALS = "checkstyle:MultipleStringLiterals";
-
-    /**
-     * Cf. https://checkstyle.sourceforge.io/config_metrics.html#BooleanExpressionComplexity
-     */
+    /** Cf. https://checkstyle.sourceforge.io/config_metrics.html#BooleanExpressionComplexity */
     public static final String BOOLEAN_EXPR_COMPLEXITY = "checkstyle:BooleanExpressionComplexity";
 
-    /**
-     * Cf. https://checkstyle.sourceforge.io/config_naming.html#MemberName
-     */
+    /** Cf. https://checkstyle.sourceforge.io/config_naming.html#MemberName */
     public static final String MEMBER_NAME = "checkstyle:MemberName";
+
+    /** Cf. https://checkstyle.sourceforge.io/config_coding.html#MultipleStringLiterals */
+    public static final String MULTIPLE_STRING_LITERALS = "checkstyle:MultipleStringLiterals";
+
+    /** Cf. https://checkstyle.sourceforge.io/checks/misc/uncommentedmain.html#UncommentedMain */
+    public static final String UNCOMMENTED_MAIN = "checkstyle:UncommentedMain";
 
     /**
      * Creates a new Checkstyle warnings class.
