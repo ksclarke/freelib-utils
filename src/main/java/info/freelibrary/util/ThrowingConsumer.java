4c717e1e305b14b8d69c322dc4937b2a58a7078b
Kevin S. Clarke <ksclarke@ksclarke.io>
2026-05-23 15:08:29 -0400

    Refactor sneaky method and Javadocs

--- src/main/java/info/freelibrary/util/ThrowingConsumer.java
+++ src/main/java/info/freelibrary/util/ThrowingConsumer.java
@@ -1,4 +1,3 @@
-
 package info.freelibrary.util;
 
 import info.freelibrary.util.warnings.JDK;
@@ -30,6 +29,18 @@ public interface ThrowingConsumer<T, E extends Exception> extends Consumer<T> {
 
     /**
      * A method that wraps any exceptions through by the consumer with a runtime exception.
+     * <p>
+     * Use this method when you want the caller to handle or declare the checked exception:
+     * <p>
+     * {@snippet lang = java:
+     * ThrowingConsumer<Path, IOException> deleter = Files::delete;
+     *
+     * try {
+     *     deleter.acceptThrows(path);
+     * } catch (final IOException details) {
+     *     // Handle the checked exception here.
+     * }
+     *}
      *
      * @param aType A type being accepted by the consumer
      * @throws E An exception thrown by the consumer
@@ -37,35 +48,57 @@ public interface ThrowingConsumer<T, E extends Exception> extends Consumer<T> {
     void acceptThrows(T aType) throws E;
 
     /**
-     * Returns a standard Consumer that rethrows checked exceptions as unchecked using a sneaky throw.
+     * Returns a standard Consumer that rethrows checked exceptions as unchecked using an unchecked throw.
+     * <p>
+     * Use this method when an API requires a standard {@link Consumer}, but the lambda or method reference can throw a
+     * checked exception, and you do not want that exception wrapped in an {@link I18nRuntimeException}:
+     * <p>
+     * {@snippet lang = java:
+     * paths.forEach(ThrowingConsumer.uncheck(path -> Files.delete(path)));
+     *}
      *
      * @param <T> The accepted type
      * @param <E> The exception type
      * @param aConsumer The ThrowingConsumer to wrap
      * @return A Consumer that rethrows checked exceptions as unchecked
      */
-    @SuppressWarnings({ PMD.AVOID_CATCHING_GENERIC_EXCEPTION })
-    static <T, E extends Exception> Consumer<T> sneaky(final ThrowingConsumer<T, E> aConsumer) {
+    @SuppressWarnings({PMD.AVOID_CATCHING_GENERIC_EXCEPTION})
+    static <T, E extends Exception> Consumer<T> uncheck(final ThrowingConsumer<T, E> aConsumer) {
         return input -> {
             try {
                 aConsumer.acceptThrows(input);
             } catch (final RuntimeException | Error details) {
                 throw details;
             } catch (final Exception details) {
-                sneakyThrow(details);
+                throwUnchecked(details);
             }
         };
     }
 
     /**
      * Returns a ThrowingConsumer that rethrows {@code I18nRuntimeException}'s cause as a checked exception.
+     * <p>
+     * Use this method when a throwing consumer may have been invoked through {@link #accept(Object)}, causing checked
+     * exceptions to be wrapped in {@link I18nRuntimeException}, and you want to expose the original checked exception
+     * again:
+     * <p>
+     * {@snippet lang = java:
+     * ThrowingConsumer<Path, IOException> deleter = Files::delete;
+     * ThrowingConsumer<Path, IOException> unwrapped = ThrowingConsumer.unwrap(deleter);
+     *
+     * try {
+     *     unwrapped.acceptThrows(path);
+     * } catch (final IOException details) {
+     *     // Handle the original checked exception here.
+     * }
+     *}
      *
      * @param <T> The accepted type
      * @param <E> The exception type
      * @param aConsumer The ThrowingConsumer to unwrap
      * @return A ThrowingConsumer that unwraps the runtime exception
      */
-    @SuppressWarnings({ JDK.UNCHECKED, PMD.PRESERVE_STACK_TRACE })
+    @SuppressWarnings({JDK.UNCHECKED, PMD.PRESERVE_STACK_TRACE})
     static <T, E extends Exception> ThrowingConsumer<T, E> unwrap(final ThrowingConsumer<T, E> aConsumer) {
         return input -> {
             try {
@@ -85,6 +118,19 @@ public interface ThrowingConsumer<T, E extends Exception> extends Consumer<T> {
     /**
      * Converts a ThrowingConsumer&lt;T, E&gt; into a standard Consumer&lt;T&gt; by wrapping exceptions in an
      * {@code I18nRuntimeException}.
+     * <p>
+     * Use this method when an API requires a standard {@link Consumer} and you want checked exceptions to be converted
+     * into {@link I18nRuntimeException}:
+     * <p>
+     * {@snippet lang = java:
+     * Consumer<Path> deleter = ThrowingConsumer.wrap(path -> Files.delete(path));
+     *
+     * try {
+     *     paths.forEach(deleter);
+     * } catch (final I18nRuntimeException details) {
+     *     // The original checked exception is available as details.getCause().
+     * }
+     *}
      *
      * @param <T> A type being accepted by the consumer
      * @param <E> The type of exception that can be caught and wrapped
@@ -96,14 +142,14 @@ public interface ThrowingConsumer<T, E extends Exception> extends Consumer<T> {
     }
 
     /**
-     * Sneakily throws a checked exception as an unchecked one.
+     * Throws a checked exception as an unchecked one.
      *
      * @param aException The exception to throw
      * @param <E> The exception type
      * @throws E The unchecked exception (will never be caught by the compiler)
      */
     @SuppressWarnings(JDK.UNCHECKED)
-    private static <E extends Throwable> void sneakyThrow(final Throwable aException) throws E {
+    private static <E extends Throwable> void throwUnchecked(final Throwable aException) throws E {
         throw (E) aException;
     }
 }
--- src/main/java/info/freelibrary/util/ThrowingRunnable.java
+++ src/main/java/info/freelibrary/util/ThrowingRunnable.java
@@ -1,4 +1,3 @@
-
 package info.freelibrary.util;
 
 import info.freelibrary.util.warnings.JDK;
@@ -23,34 +22,58 @@ public interface ThrowingRunnable<E extends Exception> {
     void run() throws E;
 
     /**
-     * Sneakily rethrows checked exceptions as unchecked without wrapping.
+     * Converts a {@code ThrowingRunnable} into a standard {@link Runnable}, allowing checked exceptions to be
+     * propagated without requiring them to be declared.
+     * <p>
+     * Use this method when an API requires a standard {@code Runnable}, but the operation can throw a checked exception
+     * and you do not want that exception wrapped in an {@link I18nRuntimeException}:
+     * <p>
+     * {@snippet lang = java:
+     * executor.execute(ThrowingRunnable.uncheck(() -> Files.delete(path)));
+     *}
+     * <p>
+     * Runtime exceptions and errors are rethrown unchanged.
      *
      * @param <E> The exception type to throw
      * @param aThrowingRunnable A throwing runnable
-     * @return A standard Runnable that sneakily throws exceptions
+     * @return A standard {@code Runnable} that propagates checked exceptions without compiler-enforced handling
      */
-    @SuppressWarnings({ PMD.AVOID_CATCHING_GENERIC_EXCEPTION })
-    static <E extends Exception> Runnable sneaky(final ThrowingRunnable<E> aThrowingRunnable) {
+    @SuppressWarnings({PMD.AVOID_CATCHING_GENERIC_EXCEPTION})
+    static <E extends Exception> Runnable uncheck(final ThrowingRunnable<E> aThrowingRunnable) {
         return () -> {
             try {
                 aThrowingRunnable.run();
             } catch (final RuntimeException | Error details) {
                 throw details;
             } catch (final Exception details) {
-                sneakyThrow(details);
+                throwUnchecked(details);
             }
         };
     }
 
     /**
-     * Unwraps a ThrowingRunnable by extracting and rethrowing the original checked exception wrapped in an
-     * {@code I18nRuntimeException}.
+     * Unwraps a {@code Runnable} by extracting and rethrowing the original checked exception wrapped in an
+     * {@link I18nRuntimeException}.
+     * <p>
+     * Use this method when a runnable may have wrapped a checked exception in {@code I18nRuntimeException}, and the
+     * caller should handle the original checked exception again:
+     * <p>
+     * {@snippet lang = java:
+     * Runnable wrapped = ThrowingRunnable.wrap(() -> Files.delete(path));
+     * ThrowingRunnable<IOException> unwrapped = ThrowingRunnable.unwrap(wrapped);
+     *
+     * try {
+     *     unwrapped.run();
+     * } catch (final IOException details) {
+     *     // Handle the original checked exception here.
+     * }
+     *}
      *
      * @param <E> The exception type to unwrap
      * @param aRunnable The wrapped runnable
-     * @return A ThrowingRunnable that rethrows the original exception
+     * @return A {@code ThrowingRunnable} that rethrows the original exception
      */
-    @SuppressWarnings({ JDK.UNCHECKED, PMD.PRESERVE_STACK_TRACE })
+    @SuppressWarnings({JDK.UNCHECKED, PMD.PRESERVE_STACK_TRACE})
     static <E extends Exception> ThrowingRunnable<E> unwrap(final Runnable aRunnable) {
         return () -> {
             try {
@@ -68,13 +91,27 @@ public interface ThrowingRunnable<E extends Exception> {
     }
 
     /**
-     * A utility to wrap ThrowingRunnable(s) and capture their checked exceptions.
+     * Converts a {@code ThrowingRunnable} into a standard {@link Runnable} that wraps checked exceptions in an
+     * {@link I18nRuntimeException}.
+     * <p>
+     * Use this method when an API requires a standard {@code Runnable} and checked exceptions should be converted into
+     * runtime exceptions:
+     * <p>
+     * {@snippet lang = java:
+     * Runnable deleter = ThrowingRunnable.wrap(() -> Files.delete(path));
+     *
+     * try {
+     *     deleter.run();
+     * } catch (final I18nRuntimeException details) {
+     *     // The original checked exception is available as details.getCause().
+     * }
+     *}
      *
      * @param <E> The exception type that is caught and wrapped in an {@code I18nRuntimeException}
      * @param aThrowingRunnable A runnable that throws checked exceptions
      * @return A runnable that turns checked exceptions into unchecked exceptions
      */
-    @SuppressWarnings({ PMD.AVOID_CATCHING_GENERIC_EXCEPTION })
+    @SuppressWarnings({PMD.AVOID_CATCHING_GENERIC_EXCEPTION})
     static <E extends Exception> Runnable wrap(final ThrowingRunnable<E> aThrowingRunnable) {
         return () -> {
             try {
@@ -86,14 +123,14 @@ public interface ThrowingRunnable<E extends Exception> {
     }
 
     /**
-     * Sneakily throws the given checked exception as an unchecked exception.
+     * Throws the given checked exception as an unchecked exception.
      *
      * @param aThrowable A checked exception
      * @param <E> The throwable type
      * @throws E Always thrown
      */
     @SuppressWarnings(JDK.UNCHECKED)
-    private static <E extends Throwable> void sneakyThrow(final Throwable aThrowable) throws E {
+    private static <E extends Throwable> void throwUnchecked(final Throwable aThrowable) throws E {
         throw (E) aThrowable;
     }
 
